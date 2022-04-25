df07b43ac0df9ffc418ea7ee9b05d7293d76098c
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2022-04-25 19:26:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let names = ["Gabi"];
+let codes = [1];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", {names: names,
+  codes: codes});
+});
+
+
+app.post("/", (req, res) => {
+  let name = req.body.name;
+
+  if (!names.includes(name)) {
+    names.push(name)
+
+    if (req.body.sat && req.body.sun) {
+      codes.push(1);
+    }
+    else if (req.body.sun) {
+      codes.push(2);
+    }
+    else if (req.body.sat) {
+      codes.push(3);
+    }
+    else if (! (req.body.sat || req.body.sun)) {
+      codes.push(4);
+    }
+  }
+
+  else if (names.includes(name)) {
+    let nameIndex = names.indexOf(name);
+    if (req.body.sat && req.body.sun) {
+      codes[nameIndex] = 1;
+    }
+    else if (req.body.sun) {
+      codes[nameIndex] = 2;
+    }
+    else if (req.body.sat) {
+      codes[nameIndex] = 3;
+    }
+    else if (! (req.body.sat || req.body.sun)) {
+      codes[nameIndex] = 4;
+    }
+  }
+
+  res.redirect("/");
+});
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running.")
+});
